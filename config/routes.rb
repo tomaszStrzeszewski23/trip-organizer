93566b63b2342031ce6786282143966ebe1021cd
Tomasz Strzeszewski <tomasz.strzeszewski@toptal.com>
2024-11-08 15:44:15 +0100

    Add devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  if Rails.env.development?
-    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
-  end
-  post "/graphql", to: "graphql#execute"
+  devise_for :users
+  mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: '/graphql' if Rails.env.development?
+  post '/graphql', to: 'graphql#execute'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
